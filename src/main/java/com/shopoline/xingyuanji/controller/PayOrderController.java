5110004139bb45a22365a84ba615f4400474d648
wutongyu987 <wu930625>
2019-02-27 22:05:39 +0800

    优化

--- src/main/java/com/shopoline/xingyuanji/controller/PayOrderController.java
+++ src/main/java/com/shopoline/xingyuanji/controller/PayOrderController.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Controller;
  * @since 2019-01-17
  */
 @Controller
-@RequestMapping("/xingyuanji/payOrder")
+@RequestMapping("/payOrder")
 public class PayOrderController {
 
 }
