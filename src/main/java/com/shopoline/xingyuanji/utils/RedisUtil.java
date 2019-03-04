8e4299e43ea95c566024ac784949e5159b376b22
wutongyu987 <wutongyu987@live.com>
2019-03-04 17:49:07 +0800

    注释

--- src/main/java/com/shopoline/xingyuanji/utils/RedisUtil.java
+++ src/main/java/com/shopoline/xingyuanji/utils/RedisUtil.java
@@ -1,13 +1,19 @@
 package com.shopoline.xingyuanji.utils;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
-
 import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class RedisUtil {
 
+    /**
+     * 1.StringRedisTemplate继承了RedisTemplate。
+     * 2.RedisTemplate是一个泛型类，而StringRedisTemplate则不是。
+     * 3.StringRedisTemplate只能对key=String，value=String的键值对进行操作，RedisTemplate可以对任何类型的key-value键值对操作。
+     * 4.他们各自序列化的方式不同，但最终都是得到了一个字节数组，殊途同归，StringRedisTemplate使用的是StringRedisSerializer类；
+     * 5.RedisTemplate使用的是JdkSerializationRedisSerializer类。反序列化，则是一个得到String，一个得到Object
+     */
     private static StringRedisTemplate stringRedisTemplate = (StringRedisTemplate) SpringUtil.getBean("stringRedisTemplate");
 
     public static void setValue(String key, String code) {
