71eb666630ed284547177cbde25ab169417c95bd
wutongyu987 <wutongyu987@live.com>
2019-03-04 17:08:12 +0800

    将token在Redis中的有效时间调整为30分（之前1小时）

--- src/main/java/com/shopoline/xingyuanji/utils/CheckTicketIdUtil.java
+++ src/main/java/com/shopoline/xingyuanji/utils/CheckTicketIdUtil.java
@@ -8,13 +8,14 @@ public class CheckTicketIdUtil {
     public LoginVO checkTicketId(String openId){
 
         String ticketId = TicketUtil.getTicketId();
+        // 如果Redis内存在当前用户的OpenId，删除
         if (RedisUtil.hasKey(openId)){
             RedisUtil.delete(RedisUtil.getValue(openId));
         }
         // 将TOKEN写入redis
         RedisUtil.setValueMinutes(ticketId, openId);
         RedisUtil.setValueMinutes(openId, ticketId);
-
+        // 将ticket与OpenId写入VO
         LoginVO loginVO = new LoginVO();
         loginVO.setTicketId(ticketId);
         loginVO.setCode(openId);
