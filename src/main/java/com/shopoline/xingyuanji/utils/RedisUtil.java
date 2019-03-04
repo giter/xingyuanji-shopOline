9bf6f4de20b78470a18f1ec07b39d8f8b6af7fc9
wutongyu987 <wutongyu987@live.com>
2019-03-04 17:53:25 +0800

    注释

--- src/main/java/com/shopoline/xingyuanji/utils/RedisUtil.java
+++ src/main/java/com/shopoline/xingyuanji/utils/RedisUtil.java
@@ -16,38 +16,82 @@ public class RedisUtil {
      */
     private static StringRedisTemplate stringRedisTemplate = (StringRedisTemplate) SpringUtil.getBean("stringRedisTemplate");
 
+    /**
+     * 设置VALUE有效时间为n天
+     * @param key
+     * @param code
+     */
     public static void setValue(String key, String code) {
         stringRedisTemplate.opsForValue().set(key, code, 1, TimeUnit.DAYS);
     }
 
+
+    /**
+     * 设置VALUE有效时间为n秒
+     * @param key
+     * @param code
+     */
     public static void setValueSeconds(String key, String code) {
         stringRedisTemplate.opsForValue().set(key, code, 900, TimeUnit.SECONDS);
     }
 
+    /**
+     * 设置VALUE有效时间为n小时
+     * @param key
+     * @param code
+     */
     public static void setValueHOURS(String key, String code) {
         stringRedisTemplate.opsForValue().set(key,code,1, TimeUnit.HOURS);
     }
 
+    /**
+     * 设置VALUE有效时间为n分钟
+     * @param key
+     * @param code
+     */
     public static void setValueMinutes(String key, String code) {
         stringRedisTemplate.opsForValue().set(key,code,30, TimeUnit.MINUTES);
     }
 
+    /**
+     * 通过key获取VALUE
+     * @param key
+     * @return
+     */
     public static String getValue(String key){
         return stringRedisTemplate.boundValueOps(key).get();
     }
 
+    /**
+     * 判断是否有某个KEY
+     * @param key
+     * @return boolean
+     */
     public static Boolean hasKey(String key) {
         return stringRedisTemplate.hasKey(key);
     }
 
+    /**
+     * 删除某个KEY
+     * @param key
+     */
     public static void delete(String key){
         stringRedisTemplate.delete(key);
     }
 
+    /**
+     * 删除key的集合
+     * @param keys
+     */
     public static void deleteKeys(Collection<String> keys){
         stringRedisTemplate.delete(keys);
     }
 
+    /**
+     * 写入Key集合
+     * @param pattern
+     * @return
+     */
     public static Set<String> setKeys(String pattern){
         return stringRedisTemplate.keys(pattern);
     }
