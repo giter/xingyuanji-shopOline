cb35ba5b7738653554b22bcba9aa22e866842934
wutongyu987 <wutongyu987@live.com>
2019-03-22 14:47:12 +0800

    123

---
+++ src/main/java/com/shopoline/xingyuanji/model/BoxProductSellCountModel.java
@@ -0,0 +1,23 @@
+package com.shopoline.xingyuanji.model;
+
+public class BoxProductSellCountModel {
+
+    private String days;
+    private String count;
+
+    public String getDays() {
+        return days;
+    }
+
+    public void setDays(String days) {
+        this.days = days;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+}
